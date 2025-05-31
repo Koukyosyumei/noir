710c150f2fb4349cad7ae8c1a95f503ef708eee0
Koukyosyumei <koukyosyumei@hotmail.com>
2025-05-30 21:58:28 -0400

    update

--- acvm-repo/brillig_vm/src/memory.rs
+++ acvm-repo/brillig_vm/src/memory.rs
@@ -397,7 +397,7 @@ impl<F: AcirField> Memory<F> {
 
         let max_memory_size_env =
             std::env::var("ZKFUZZ_NOIR_MAX_MEMORY_SIZE").unwrap_or_else(|_| "1000000".to_string());
-        let max_memory_size_usize = seed_env.parse::<usize>().unwrap_or(1000000);
+        let max_memory_size_usize = max_memory_size_env.parse::<usize>().unwrap_or(1000000);
 
         if new_size > max_memory_size_usize {
             panic!("too large memory allocation of {} bytes", new_size);
